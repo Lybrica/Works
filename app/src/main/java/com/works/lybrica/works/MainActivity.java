03b73ad6bc856dd40282fafed7904f6734d98479
Lybrica <contact.lybrica@gmail.com>
2016-04-27 16:23:09 +0300

    Test
    asd

--- app/src/main/java/com/works/lybrica/works/MainActivity.java
+++ app/src/main/java/com/works/lybrica/works/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+		String asd = test;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
